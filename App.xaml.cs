5fd871d14a0a76cd5c958aa504f5ebb6b2b7d40c
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-07-02 14:41:55 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace People
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
