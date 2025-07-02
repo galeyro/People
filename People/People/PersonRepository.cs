eec1dcc2dc0ee364d4fcbd57794182b25d7a4ccd
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-07-02 14:58:51 -0500

    Retrieve rows from the database

--- People/People/PersonRepository.cs
+++ People/People/PersonRepository.cs
@@ -56,7 +56,8 @@ public class PersonRepository
         // TODO: Init then retrieve a list of Person objects from the database into a list
         try
         {
-            
+            Init();
+            return conn.Table<Person>().ToList();
         }
         catch (Exception ex)
         {
